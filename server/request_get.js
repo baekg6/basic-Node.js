5c5e4b033a715217de1d54340acffb0576278e56
baek gyeonghyeon <baekg.dev@gmail.com>
2023-02-02 14:47:54 +0900

    Docs: get 요청 분석하기

---
+++ server/request_get.js
@@ -0,0 +1,21 @@
+let http = require('http');
+
+// 1. 요청 url 처리를 위하여 url 모듈, querystring 모듈 사용
+let url = require('url');
+var querystring = require('querystring');
+
+let server = http.createServer(function (request, response) {
+  // 2. 브라우저 요청 주소를 파싱하여 객체화
+  let parsedUrl = url.parse(request.url);
+  console.log('parsedUrl > ', parsedUrl);
+
+  let parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
+  console.log('parsedQuery > ', parsedQuery);
+
+  response.writeHead(200, { 'Content-type': 'text/plain' });
+  response.end('Hello world!\n');
+});
+
+server.listen(8000, () => {
+  console.log('Server running at http://localhost:8000');
+});
